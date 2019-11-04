07282060f66cc948bbcef057f50424500c10e0e3
MuYu <mr.muzea@gmail.com>
2019-11-04 17:42:13 +0800

    build

---
+++ precache-manifest.aaf24a0f8b824ab53cc82c12037510c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "579b4603a8592ba374c68a4ae3533f39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cf132c4480a96e300c9",
+    "url": "/static/css/main.bdbdd148.chunk.css"
+  },
+  {
+    "revision": "e062bb95b9c8b1e9c72e",
+    "url": "/static/js/2.1139f550.chunk.js"
+  },
+  {
+    "revision": "2cf132c4480a96e300c9",
+    "url": "/static/js/main.d080ef9c.chunk.js"
+  },
+  {
+    "revision": "99b2360ddcd6f6a84129",
+    "url": "/static/js/runtime-main.6f452a70.js"
+  }
+]);
